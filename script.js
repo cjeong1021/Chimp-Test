c58850525a8fa00a79a5ff75b158c7f8349ec6e7
Christopher <cjeong1021@gmail.com>
2022-05-13 15:54:39 -0400

    remove eventListener on click and create win/lose condition

--- script.js
+++ script.js
@@ -5,14 +5,29 @@ let gridArray = [   // Grid representing gamestate
     6,7,8
 ];
 
-grid.forEach((box) => {
-    box.addEventListener('click', () => {
-        box.style.backgroundColor = 'rgb(77, 169, 169)';
+document.body.addEventListener('click', () => {
+    grid.forEach((box) => {
+        box.innerText = "";
     })
 })
 
 let gridPlacement = generateRandom();
+let clicks = 0;
 placeNumbers();
+
+grid.forEach((box) => {
+    box.addEventListener('click', checkClick)
+})
+
+function checkClick(e) { //Checks if box is clicked in correct order. Changes color on correct, alerts on incorrect
+    if (e.target.id == gridPlacement[clicks]) {
+    e.target.style.backgroundColor = 'rgb(77, 169, 169)';
+    } else {
+        alert("you lose")
+    }
+    clicks++;
+    e.target.removeEventListener('click', checkClick)
+}
 
 function placeNumbers() {   // Place numbers randomly on grid
     for (let i = 0; i < gridPlacement.length; i++) {
@@ -24,7 +39,7 @@ function placeNumbers() {   // Place numbers randomly on grid
 
 function generateRandom() { // Generate random array to place numbers on grid
     let randomArray = [];
-    for (let i = 0; i < Math.floor(gridArray.length/4); i++) {
+    for (let i = 0; i < Math.floor(gridArray.length/3); i++) {
         let randomNumber = Math.floor(Math.random() * gridArray.length);
         
         while (randomArray.includes(randomNumber) === true) {

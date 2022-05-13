617018959a740efb8cc49ed2d4923411e930deb0
Christopher <cjeong1021@gmail.com>
2022-05-13 14:51:19 -0400

    add generateRandom function to generate random array for grid placement

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ grid.forEach((box) => {
         box.style.backgroundColor = 'rgb(77, 169, 169)';
     })
 })
+
+function generateRandom() { // Generate random array to place numbers on grid
+    let randomArray = [];
+    for (let i = 0; i < Math.floor(gridArray.length/3); i++) {
+        let randomNumber = Math.floor(Math.random() * gridArray.length);
+        
+        while (randomArray.includes(randomNumber) === true) {
+            randomNumber = Math.floor(Math.random() * gridArray.length);
+        }
+        randomArray.push(randomNumber);
+    }
+    return randomArray;
+}
